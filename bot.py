d79a7a420fa9568053a0aa87a5829dca67a13bb2
Eugene Pisarev <91762157+Eugene571@users.noreply.github.com>
2024-10-11 15:29:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,26 +4,27 @@ import time
 from dotenv import load_dotenv, find_dotenv
 import argparse
 from random import choice
+from telegram.error import TelegramError, NetworkError
 
 
-def pics_to_post(directory):
+def post_pics(directory):
     return [os.path.join(directory, img) for img in os.listdir(directory) if img.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))]
 
 
-def send_photo_to_channel(bot, chat_id, photo_path):
+def try_to_send_photo(bot, chat_id, photo_path):
     try:
         with open(photo_path, 'rb') as img:
             bot.send_photo(chat_id=chat_id, photo=img)
         print(f"Posted {photo_path} to the channel.")
-    except Exception as e:
+    except (FileNotFoundError, TelegramError) as e:
         print(f"Error posting {photo_path}: {e}")
 
 
-def send_text_to_channel(bot, chat_id, text):
+def try_to_send_text(bot, chat_id, text):
     try:
         bot.send_message(chat_id=chat_id, text=text)
         print(f"Posted text: '{text}' to the channel.")
-    except Exception as e:
+    except TelegramError as e:
         print(f"Error posting text: {e}")
 
 
@@ -38,20 +39,20 @@ def main():
     args = parser.parse_args()
 
     load_dotenv(find_dotenv())
-    bot = telegram.Bot(token=os.getenv('TG_API_KEY'))
-    chat_id = os.getenv('TG_CHAT_ID')
+    bot = telegram.Bot(token=os.environ['TG_API_KEY'])
+    chat_id = os.environ['TG_CHAT_ID']
 
     post_freq = args.time if args.time is not None else int(os.getenv('POST_FREQ', 14400))
 
     if args.text:
-        send_text_to_channel(bot, chat_id, args.text)
+        try_to_send_text(bot, chat_id, args.text)
 
-    images = pics_to_post(args.path)
+    images = post_pics(args.path)
 
     if args.image:
         image_path = args.image if os.path.isabs(args.image) else os.path.join(args.path, args.image)
         if os.path.exists(image_path):
-            send_photo_to_channel(bot, chat_id, image_path)
+            try_to_send_photo(bot, chat_id, image_path)
         else:
             print(f"Image {image_path} does not exist.")
         return
@@ -65,8 +66,16 @@ def main():
             break
 
         image = choice(available_images)
-        send_photo_to_channel(bot, chat_id, image)
-        posted_images.add(image)
+
+        while True:
+            try:
+                try_to_send_photo(bot, chat_id, image)
+                posted_images.add(image)
+                break
+            except (NetworkError, TelegramError) as e:
+                print(f"Network error occurred: {e}. Retrying in 5 seconds...")
+                time.sleep(5)
+
         time.sleep(post_freq)
 
 
